b15a2b32d88cab8e04e557c93cacf558bc1cef3c
zelteb <helpzelteb@gmail.comm>
2026-03-04 21:58:09 +0530

    update

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -502,7 +502,7 @@ export default function Watch({ params }: { params: Promise<{ id: string }> }) {
                 <line x1="3" y1="6" x2="21" y2="6"/>
                 <path d="M16 10a4 4 0 0 1-8 0"/>
               </svg>
-              Start Selling Now
+              Sell your own product
             </a>
           </div>
         </div>
