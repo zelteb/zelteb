c953d5484a0c9626acde3740054bfd1252160715
zelteb <helpzelteb@gmail.comm>
2026-03-07 11:42:55 +0530

    update

--- app/Refund/page.tsx
+++ app/Refund/page.tsx
@@ -187,11 +187,9 @@ export default function RefundPolicyPage() {
               <Link href="/" className="hover:text-gray-400">Home</Link>
               <Link href="/about" className="hover:text-gray-400">About</Link>
               <Link href="/pricing" className="hover:text-gray-400">Pricing</Link>
-              <Link href="#" className="hover:text-gray-400">Features</Link>
-              <Link href="#" className="hover:text-gray-400">Blog</Link>
             </div>
             <div className="flex flex-col space-y-5 text-lg font-medium">
-              <Link href="#" className="hover:text-gray-400">Help</Link>
+              <Link href="help" className="hover:text-gray-400">Help</Link>
               <Link href="/terms" className="hover:text-gray-400">Terms of Service</Link>
               <Link href="/privacy" className="hover:text-gray-400">Privacy Policy</Link>
               <Link href="/refund" className="font-bold border-b-2 border-[#f398e4] w-fit">Refund Policy</Link>
