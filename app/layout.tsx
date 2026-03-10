4cb7987ba2c929d12aa926a2d9261db1d2c3008d
zelteb <helpzelteb@gmail.comm>
2026-03-10 17:06:42 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,6 +99,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3DWEE70CWM" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3DWEE70CWM');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
