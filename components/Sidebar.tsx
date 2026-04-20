23f50634e52c2216057de8976f7e302355aaf5cf
zelteb <helpzelteb@gmail.comm>
2026-04-20 17:19:36 +0530

    update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -49,7 +49,7 @@ export default function Sidebar() {
   const sidebarItems = [
     { name: "Home", icon: "🏠", link: "/dashboard" },
     { name: "Payouts", icon: "🏦", link: "/dashboard/payouts" },
-    { name: "Social Media", icon: "🔗", link: "/dashboard/social_media" },
+    { name: "Social Media", icon: "🔗", link: "/dashboard/Requests" },
     { name: "Profile", icon: "👤", link: "/dashboard/profile" },
     ...(username
       ? [{ name: "View My Page", icon: "🌐", link: `/${username}` }]
